16964a1e51aa2543f264c7e602a45be5ec618508
iltifatyousuf <iltifaatyousuf@gmail.com>
2026-09-14 21:51:20 +0530

    Fix CompareButton import path in EquipmentCard

--- src/components/ui/EquipmentCard.tsx
+++ src/components/ui/EquipmentCard.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import { formatPrice } from '@/lib/utils';
-import CompareButton from './CompareButton';
+import CompareButton from '@/components/equipment/CompareButton';
 
 interface EquipmentCardProps {
   equipment: any;
